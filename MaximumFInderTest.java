71e0b6756f2534961c19b36d3c8205bcc90c6682
Charalampos Mparounis <Divisionby0@outlook.com.gr>
2017-10-15 14:07:02 +0300

    Simple java excercise to test github config on personal system.

---
+++ MaximumFInderTest.java
@@ -0,0 +1,9 @@
+//MaximumFinderTest, a class to apply MaximumFinder.java
+//This is not a part of the project, but a tutorial for personal use
+
+public class MaximumFinderTest {
+  public static void main(String[] args) {
+    MaximumFinder max1 = new MaximumFinder();
+    max1.determineMaximum();
+  }
+}
