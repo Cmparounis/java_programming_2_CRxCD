6297d5fbb08618466cb5e8edb5e9030e11ba3829
Charalampos Mparounis <Divisionby0@outlook.com.gr>
2017-11-16 15:53:29 +0200

    develop ParseElement and App

--- src/main/java/com/crxcd/app/App.java
+++ src/main/java/com/crxcd/app/App.java
@@ -1,7 +1,6 @@
-package com.crxcd.app;
-
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Spellchecker by CRxCD- in development" );
+        ParseElement.reader();
     }
 }
--- src/main/java/com/crxcd/app/ParseElement.java
+++ src/main/java/com/crxcd/app/ParseElement.java
@@ -1,45 +1,50 @@
 //Program that reads and imports user input
+
 import java.util.Scanner; //Imports class Scanner
 import java.io.BufferedReader; //Imports class BufferedReader
 import java.io.FileReader; //Imports class FileReader
 import java.io.IOException; //Imports class IOException
-
-package com.crxcd.app;
+import java.nio.file.Files;//Imports class Files
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
 
 public class ParseElement {
-  public static Array<String> reader() {
+  public static String[] reader() {
     //method reader() parses the input, decides whether the input is a path for a
     //file or raw text and then appends every line to the Array fileArray,
     //which it returns
-    String input;
-    System.out.println("Type your text or the path of the file you would like
-     to be processed:");//prompt user
+    System.out.println("Type your text or the path of the file you would like to be processed:");//prompt user
     Scanner sc = new Scanner(System.in);
-    String input = scanner.nextLine();
-    if (isFilePath(input) = true){
-      try (BufferedReader reader = new Files.newBufferedReader(input, "UTF-8")) {
-        String sCurrentLine = null;
-        String[][] wordArray;
-        int i = 0;
-        while ((sCurrentLine =reader.readLine()) != null) {
-          wordArray[i][] = sCurrentLine.split(" ");
-          for (String word : arr) {
-            System.out.println(word);
-          }
-          i++;
-        }
-      } catch (IOException e) {
-        System.err.format.("IOException: %s%n", x);
-      }
-      System.out.println("Your file was initialized.");
-    } else {
+    String input1 = sc.nextLine();
+    String[] wordArray;
 
+    if (isFilePath(input1) == true){
+	  Path file = Paths.get(input1);
+	  wordArray = new String[10];
+	  try (BufferedReader reader = Files.newBufferedReader(file, StandardCharsets.UTF_8)) {
+		String sCurrentLine = null;
+		int i = 0;
+		while ((sCurrentLine =reader.readLine()) != null) {
+		  wordArray = sCurrentLine.split(" ");
+		  for (String word : wordArray) {
+			System.out.println(word);
+		  }
+		  i++;
+		}
+	  } catch (IOException e) {
+		System.err.format("IOException: %s%n", e);
+	  }
+	  System.out.println("Your file was initialized.");
+	  return wordArray;
+    } else {
+		wordArray = new String[10];
       System.out.println("Your input was initialized.");
+      return wordArray;
     }
 
   }
-  public static boolean isFilePath(String input) {
-
+  public static boolean isFilePath(String str) {
+    return true;
   }
-
 }
