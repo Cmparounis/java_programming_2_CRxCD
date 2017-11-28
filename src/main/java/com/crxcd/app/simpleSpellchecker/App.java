612459df23157d47a52b36c9978cc6a9289ce4a4
manosalexiou95 <32976992+manosalexiou95@users.noreply.github.com>
2017-11-28 19:07:51 +0200

    updating repository

---
+++ src/main/java/com/crxcd/app/simpleSpellchecker/App.java
@@ -0,0 +1,31 @@
+package com.CRxCD.app.simpleSpellchecker;
+
+import java.util.Scanner;
+/**
+ * <h1> Type or quit</h1>
+ * 
+ * @author Haris Barounis
+ */
+
+import util.ParseElement;
+
+public class App {
+	private static Scanner sc;
+
+	public static void main(String[] args) {
+		System.out.println("Spellchecker by CRxCD- in development");
+  		String input1 = " " ;
+  		while (input1.compareToIgnoreCase("quit") != 0) {
+          	System.out.println("Type 'Quit' in order to quit or type your text or the path of the file you would like to be processed:");//prompt user
+  			sc = new Scanner(System.in);
+  			input1 = sc.nextLine();
+  			while (input1.isEmpty()) {
+  				System.out.println("You must type at least one word.");
+  				input1 = sc.nextLine();
+  			}
+  			if (input1.compareToIgnoreCase("quit") != 0) {
+  				ParseElement.reader(input1);
+  			}
+      }
+    }
+}
