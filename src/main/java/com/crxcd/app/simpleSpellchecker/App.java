19d2afd7387920e30fb8a9dd3e0a1ae71669cc09
Charalampos Barounis <Divisionby0@outlook.com.gr>
2017-12-12 20:40:11 +0200

    trying to resolve problems

--- src/main/java/com/crxcd/app/simpleSpellchecker/App.java
+++ src/main/java/com/crxcd/app/simpleSpellchecker/App.java
@@ -1,17 +1,23 @@
-package com.CRxCD.app.simpleSpellchecker;
-
+package com.crxcd.app.simpleSpellchecker;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Scanner;
 
+import util.Checker;
+import util.DictionaryGenerator;
 import util.ParseElement;
 
 public class App {
 	private static Scanner sc;
-
+	private static final String DictionaryPath =  new File("src/main/java/config/greek.txt").getAbsolutePath();
 	public static void main(String[] args) {
-		System.out.println("Spellchecker by CRxCD- in development");
+		System.out.println("Greek Spellchecker by CRxCD- in development");
   		String input1 = " " ;
   		while (input1.compareToIgnoreCase("quit") != 0) {
-          	System.out.println("Type 'Quit' in order to quit or type your text or the path of the file you would like to be processed:");//prompt user
+          	System.out.println("Type your text or the path of the file you would like to be processed:");
+          	System.out.println("Type 'Quit' in order to quit.");
   			sc = new Scanner(System.in);
   			input1 = sc.nextLine();
   			while (input1.isEmpty()) {
@@ -19,8 +25,17 @@ public class App {
   				input1 = sc.nextLine();
   			}
   			if (input1.compareToIgnoreCase("quit") != 0) {
-  				ParseElement.reader(input1);
+  				try {
+  					final long startTime = System.nanoTime();
+  					LinkedList<String> uInput = new LinkedList<String>(ParseElement.reader(input1));
+  					HashSet<String> grDictionary = new HashSet<String>(DictionaryGenerator.readFile(DictionaryPath));
+  					Checker.finder(uInput, grDictionary) ;
+  					final long endTime = System.nanoTime();
+  					System.out.println("The process took : " + ((endTime - startTime) / 1000000) + "ms to complete.");
+  				} catch (FileNotFoundException e) {
+  					System.out.println(e.getMessage());
+  				}
   			}
-      }
+  		}
     }
 }
--- src/main/java/util/ParseElement.java
+++ src/main/java/util/ParseElement.java
@@ -1,60 +1,44 @@
 package util;
 
-import java.util.LinkedList;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.charset.StandardCharsets;
+import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ParseElement {
 	private static final Pattern UNWANTED_SYMBOLS = Pattern.compile("\\p{Punct}");
-	public static LinkedList<String> reader(String input1) {
-		String[] wordArray;
-		LinkedList<String> wordList = new LinkedList<String>();
+	private static final Pattern FILE_PATH = Pattern.compile("([A-Z|a-z]:\\\\[^*|\"<>?\\n]*)|(\\\\\\\\.*?\\\\.*)");
+	public static LinkedList<String> reader(String input1) throws FileNotFoundException {
 		if (isFilePath(input1)){
+			if (!fileExists(input1)) {
+				throw new FileNotFoundException("Please submit a valid path.");
+			}
 			Path file = Paths.get(input1);
-			wordArray = new String[50];
+			return fileParser(file);
+		} else {
+			Path file = null;
 			try {
-				BufferedReader reader = Files.newBufferedReader(file, StandardCharsets.UTF_8);
-				String sCurrentLine = null;
-				while ((sCurrentLine =reader.readLine()) != null) {
-					Matcher unwantedMatcher = UNWANTED_SYMBOLS.matcher(sCurrentLine);
-					sCurrentLine = unwantedMatcher.replaceAll("");
-					wordArray = sCurrentLine.split(" ");
-					for (int i = 0; i < wordArray.length; i++) {
-						wordList.add(wordArray[i]);
-					}
-				}
-				for (int z = 0; z < wordList.size(); z++) {
-					System.out.println(wordList.get(z));
-					}
-				System.out.println(wordList.size());
+				File temp = File.createTempFile("temp-input", ".tmp");
+				BufferedWriter writer = new BufferedWriter(new FileWriter(temp));
+				writer.write(input1);
+				writer.close();
+				file = Paths.get(temp.getAbsolutePath());
 			} catch (IOException e) {
 				System.err.format("IOException: %s%n", e);
 			}
-			System.out.println("Your file was initialized.");
-			return wordList;
-		} else {
-			wordArray = new String[50];
-			Matcher unwantedMatcher = UNWANTED_SYMBOLS.matcher(input1);
-			input1 = unwantedMatcher.replaceAll("");
-			wordArray = input1.split(" ");
-			for (int i = 0; i < wordArray.length; i++) {
-				wordList.add(wordArray[i]);
-			}
-			for (int z = 0; z < wordList.size(); z++) {
-				System.out.println(wordList.get(z));
-			}
-			System.out.println("Your input was initialized.");
-			return wordList;
+			return fileParser(file);
 		}
 	}
-	public static boolean isFilePath(String str) {
+	public static boolean fileExists(String str) {
 		File f = null;
 		boolean checker = false;
 		try{
@@ -64,5 +48,44 @@ public class ParseElement {
 			e.printStackTrace();
 		}
 		return checker;
+	}
+	public static boolean isFilePath(String str) {
+		Matcher pathMatcher = FILE_PATH.matcher(str);
+		if (pathMatcher.matches()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public static LinkedList<String> fileParser(Path file) {
+		boolean success = true;
+		String[] wordArray = new String[100];
+		LinkedList<String> wordList = new LinkedList<String>();
+		try {
+			BufferedReader reader = Files.newBufferedReader(file , StandardCharsets.UTF_8);
+			String sCurrentLine = null;
+			while ((sCurrentLine =reader.readLine()) != null) {
+				Matcher unwantedMatcher = UNWANTED_SYMBOLS.matcher(sCurrentLine);
+				sCurrentLine = unwantedMatcher.replaceAll("");
+				wordArray = sCurrentLine.split(" ");
+				for (int i = 0; i < wordArray.length; i++) {
+					wordList.add(wordArray[i]);
+				}
+			}
+			for (int z = 0; z < wordList.size(); z++) {
+				System.out.println(wordList.get(z));
+				}
+			System.out.println(wordList.size());
+		} catch (IOException e) {
+			System.err.format("IOException: %s%n", e);
+			success = false;
+		} finally {
+			if (!success) {
+				System.out.println("Your input was not initialized.");
+			} else {
+				System.out.println("Your input was initialized.");
+			}
+		}
+		return wordList;
 	}
 }
