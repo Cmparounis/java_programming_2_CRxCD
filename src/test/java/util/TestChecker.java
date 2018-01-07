aa8424125887b8674bd8b4a97421808f662d4a1b
Cmparounis <divisionby0@outlook.com.gr>
2018-01-07 14:08:19 +0200

    reentering development
    test commit to check intergrity

--- src/test/java/util/TestChecker.java
+++ src/test/java/util/TestChecker.java
@@ -1,13 +1,13 @@
 package util;
+import java.util.HashSet;
 import java.util.LinkedList;
 
 import org.junit.Test;
 
-import java.util.HashSet;
 import junit.framework.TestCase;
 
 public class TestChecker extends TestCase {
-	
+
 	@Test
 	public void test() {
 		LinkedList<String> testList = new LinkedList<String>();
@@ -20,11 +20,11 @@ public class TestChecker extends TestCase {
 		testHash.add("αυτοκίνητο");
 		testHash.add("Τραπέζι");
 		testHash.add("δεκα");
-		testHash.add("υπολογιστής");		
-		resultsList.add("υπολοιστής");	
-		
+		testHash.add("υπολογιστής");
+		resultsList.add("υπολοιστής");
+
 		assertEquals(resultsList, Checker.finder(testList, testHash));
-		
+
 	}
 
 }
